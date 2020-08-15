2a812d32fc03af5d57625e149f4a6178a65c5a08
Zedd1558 <zahidulislam@iut-dhaka.edu>
2020-08-15 19:21:12 +0600

    image viewer complete

--- image_inpainter.py
+++ image_inpainter.py
@@ -1,7 +1,7 @@
 from PyQt5.QtWidgets import QMainWindow, QLabel, QFileDialog, QFrame, QApplication, QPushButton, QTextEdit, QMessageBox
 from PyQt5 import uic
 from PyQt5.QtCore import Qt
-#from editpage import Editpage
+from editpage import Editpage
 import sys
 import resources
 
@@ -24,23 +24,31 @@ class Homepage(QMainWindow):
 		self.setWindowFlags(flags)
 		self.setAttribute(Qt.WA_TranslucentBackground)
 		self.show()
-		
+
 	def mousePressEvent(self, event):
-		self.offset = event.pos()
+		if event.button() == Qt.LeftButton:
+			self.offset = event.pos()
+		else:
+			super().mousePressEvent(event)
 
 	def mouseMoveEvent(self, event):
-		x = event.globalX()
-		y = event.globalY()
-		x_w = self.offset.x()
-		y_w = self.offset.y()
-		self.move(x - x_w, y - y_w)
+		if self.offset is not None and event.buttons() == Qt.LeftButton:
+			self.move(self.pos() + event.pos() - self.offset)
+		else:
+			super().mouseMoveEvent(event)
+
+	def mouseReleaseEvent(self, event):
+		self.offset = None
+		super().mouseReleaseEvent(event)	
+
 
 	def add_image(self):
-		fileName, _ = QFileDialog.getOpenFileName(None, "Open image file...")
-		print(fileName)
-		#editpage = Editpage(fileName)
-		pass
-
+		image_path, _ = QFileDialog.getOpenFileName(None, "Open image file...")
+		print(image_path)
+		self.editpage = Editpage(self, image_path)
+		self.hide()
+		self.editpage.show()
+		
 
 	
 if __name__ == "__main__":
@@ -53,6 +61,17 @@ if __name__ == "__main__":
 
 
 '''
+	def _mousePressEvent(self, event):
+		self.offset = event.pos()
+
+	def _mouseMoveEvent(self, event):
+		x = event.globalX()
+		y = event.globalY()
+		x_w = self.offset.x()
+		y_w = self.offset.y()
+		self.move(x - x_w, y - y_w)
+
+
 		buttonReply = QMessageBox.question(self, 'test message', "Do you like PyQt5?", QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
 		if buttonReply == QMessageBox.Yes:
 			print("Yes")
